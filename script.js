04909d3d2895e9247022fad353c01aee4e80e514
MeshalS <meshal.saus@gmail.com>
2020-09-24 08:08:53 +0300

    getting the past-city and weather click

--- script.js
+++ script.js
@@ -5,4 +5,27 @@ $(document).ready(function () {
     let requestType = ""; 
     let query ="";
 
+// pull current location
+$('#getWeather,#past-cities').on('click', function () {
+    
+    // get location from user input box
+    //get the first click event 
+    let e = $(event.target)[0];
+    let location = "";
+// trying to know which one of the selector from the click 
+    if (e.id == "getWeather" ) {  
+      location = $('#city-search').val().trim().toUpperCase();
+      
+    } 
+    else if ( e.className === ("cityList") ) 
+    {
+      location = e.innerText;
+    }
+
+    // should make this generic to use this on the area clicked
+    // let location = $(this).val().trim().toUpperCase();
+    if (location == "") return;
+
+
+
 })
