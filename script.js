d1172c7e0d9ea1ba8bfd9ab61101119109972864
MeshalS <meshal.saus@gmail.com>
2020-09-24 08:07:39 +0300

    adding the Jqurydoc

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    // use school key inorder to get forecast data
+    const apiKey = "166a433c57516f51dfab1f7edaed8413";
+    let url = 'https://api.openweathermap.org/data/2.5/';
+    let requestType = ""; 
+    let query ="";
+
+})
